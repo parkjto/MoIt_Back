beaf3ae2969405f7198c041b88538c650aa25b8a
crepas <kimsw021104@gmail.com>
2025-10-31 23:02:33 +0900

    start 얼마버니 project

---
+++ src/main/java/com/project/eolmabuny/EolmabunyApplication.java
@@ -0,0 +1,13 @@
+package com.project.eolmabuny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EolmabunyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EolmabunyApplication.class, args);
+	}
+
+}
